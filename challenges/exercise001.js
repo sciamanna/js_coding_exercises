74343fa7412b6ae73152cf86d124c45161204738
sciamanna <rebeca.sciamanna@me.com>
2020-11-27 11:38:57 +0000

    Completed exercise 4

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,43 +1,43 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  return word.substr(0,1).toUpperCase() + word.substr(1);
+  return word.substr(0, 1).toUpperCase() + word.substr(1);
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  return firstName.substr(0,1)+"."+lastName.substr(0,1);
+  return firstName.substr(0, 1) + "." + lastName.substr(0, 1);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
- // return (originalPrice += originalPrice*vatRate/100).toFixed(2);
- let num = originalPrice += originalPrice*vatRate/100;
- return Math.round( num * 100 + Number.EPSILON ) / 100;
+  // return (originalPrice += originalPrice*vatRate/100).toFixed(2);
+  let num = originalPrice += originalPrice * vatRate / 100;
+  return Math.round(num * 100 + Number.EPSILON) / 100;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   let num = originalPrice - (originalPrice * reduction / 100)
-  return Math.round( num * 100 + Number.EPSILON ) / 100;
+  return Math.round(num * 100 + Number.EPSILON) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
   let rem = str.length % 2;
   let pos = parseInt(str.length / 2);
-  if (rem > 0){
+  if (rem > 0) {
     return str[pos];
   }
-  return str[pos-1] + str [pos]
+  return str[pos - 1] + str[pos]
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   let rev = "";
-  for (let i = word.length-1; i>=0; i--){
+  for (let i = word.length - 1; i >= 0; i--) {
     rev += word[i];
   }
   return rev;
@@ -45,8 +45,8 @@ function reverseWord(word) {
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  for (let i = 0; i < words.length; i++){
-    words[i] = reverseWord( words[i] );
+  for (let i = 0; i < words.length; i++) {
+    words[i] = reverseWord(words[i]);
   }
   return words;
 }
@@ -54,8 +54,8 @@ function reverseAllWords(words) {
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
   let count = 0;
-  for (let i = 0; i < users.length; i++){
-    if (users[i].type === "Linux" ){
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].type === "Linux") {
       count++
     }
   }
@@ -65,19 +65,19 @@ function countLinuxUsers(users) {
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
   let sum = 0;
-  for (let i = 0; i < scores.length; i++){
+  for (let i = 0; i < scores.length; i++) {
     sum += scores[i];
   }
-  let num =  sum / scores.length;
-  return Math.round( num * 100 + Number.EPSILON ) / 100;
+  let num = sum / scores.length;
+  return Math.round(num * 100 + Number.EPSILON) / 100;
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-    if (n % 3 === 0 && n % 5 ===0) return 'fizzbuzz';
-    if (n % 3 === 0) return 'fizz';
-    if (n % 5 === 0) return 'buzz';
-    else return n;
+  if (n % 3 === 0 && n % 5 === 0) return 'fizzbuzz';
+  if (n % 3 === 0) return 'fizz';
+  if (n % 5 === 0) return 'buzz';
+  else return n;
 }
 
 module.exports = {
--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,43 +1,97 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  const smallNums = nums.filter(function (num) {
+    if (num < 1) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return smallNums;
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  const newNames = names.filter(function (name) {
+    if (name.startsWith(char)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return newNames;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  const verbs = words.filter(function (word) {
+    if (word.startsWith('to ')) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return verbs;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  const ints = nums.filter(function (num) {
+    if (Number.isInteger(num)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return ints;
+
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  const cities = [];
+  users.forEach(user => {
+    const city = user.data.city.displayName;
+    cities.push(city);
+  });
+  return cities;
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  const sqRoots = [];
+  nums.forEach(num => {
+    const root = Math.sqrt(num);
+    const sqRoot = Math.round(root * 100 + Number.EPSILON) / 100;
+    sqRoots.push(sqRoot);
+  });
+  return sqRoots;
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+    const newSentences = sentences.filter(function (sentence) {
+      const lSentence = sentence.toLowerCase();
+      const lStr = str.toLowerCase();
+      if (lSentence.includes(lStr)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return newSentences;    
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+    const lsides = [];
+    triangles.forEach(triangle => {
+    const lSide = Math.max(...triangle);
+    lsides.push(lSide);
+  });
+  return lsides;
 }
 
 module.exports = {
